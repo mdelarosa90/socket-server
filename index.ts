8baf3d39db0ad552ae721d68773b645db5d6132b
Michel De la Rosa <michel.delarosa@pluriza.com>
2018-11-30 09:21:43 -0500

    seccion 4

--- index.ts
+++ index.ts
@@ -9,14 +9,14 @@ import cors from 'cors';
 
 
 
-const server = new Server ();
+const server = Server.instance;
 
 //Body Parser
 server.app.use( bodyParser.urlencoded({extended: true}) );
 server.app.use( bodyParser.json() );
 
 //CORS
-server.app.use( cors({ origin: true, credentials: true  }));
+server.app.use( cors({ origin: true, credentials: true }));
 
 
 //Rutas de Servicios
--- routes/router.ts
+++ routes/router.ts
@@ -3,11 +3,11 @@ import { Router, Request, Response } from 'express'
 const router = Router ();
 
 router.get('/mensajes', (req: Request, res: Response) => {
-
+    
     res.json({
         ok:true,
         mensaje: 'Todo esta bien!!'
-    })
+    }) 
 
 
 })
